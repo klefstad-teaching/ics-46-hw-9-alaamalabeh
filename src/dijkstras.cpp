e4d378a5a0fa85071dd0224102d16bc2d3aa5735
alaamalabeh <malabeha@uci.edu>
2025-03-14 20:56:31 +0000

    fixed dijkstras

--- src/dijkstras.cpp
+++ src/dijkstras.cpp
@@ -69,27 +69,3 @@ void print_path(const vector<int>& path, int total) {
     }
     cout << endl;
 }
-
-int main() {
-    Graph G;
-    string filename = "small.txt";
-
-    try {
-        file_to_graph(filename, G);
-    } catch (const exception& e) {
-        cerr << e.what() << endl;
-        return 1;
-    }
-
-    int source = 0;
-    vector<int> previous(G.numVertices, -1);
-
-    vector<int> distances = dijkstra_shortest_path(G, source, previous);
-
-    for (int i = 1; i < G.numVertices; i++) {
-        vector<int> path = extract_shortest_path(distances, previous, i);
-        print_path(path, distances[i]);
-    }
-
-    return 0;
-}
