9581e47ec3abc8dc306338e11bea68655fb55f8d
alaamalabeh <malabeha@uci.edu>
2025-03-14 21:03:11 +0000

    fixed print function

--- src/dijkstras.cpp
+++ src/dijkstras.cpp
@@ -58,14 +58,12 @@ vector<int> extract_shortest_path(const vector<int>& distances, const vector<int
 
 void print_path(const vector<int>& path, int total) {
     if (path.empty()) {
-        cout << "No path available.\n";
+        cout << "\nTotal cost is " << total << endl;
         return;
     }
-
-    cout << "Total cost is" << total;
     for (size_t i = 0; i < path.size(); i++) {
         cout << path[i];
         if (i < path.size() - 1) cout << " ";
     }
-    cout << endl;
+    cout << "\nTotal cost is " << total << endl;
 }
